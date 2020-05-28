a12603d4db17932cb28a54dd912e360b41a19f1e
dexterIL <58058227+dexterIL@users.noreply.github.com>
2020-05-27 22:20:27 -0700

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -12,7 +12,7 @@ public:
 	Complex operator-(const Complex&) const; // subtraction
 	Complex operator*(const Complex&) const; // multiplication
 	Complex operator/(const Complex&) const; // division
-	
+
 	explicit Complex(double realPart = 0.0 , double imaginaryPart = 0.0 )
 	{
 		real = realPart;
@@ -37,8 +37,10 @@ public:
 	{
 		return  Complex{ (real * c.real) - (imaginary * c.imaginary)
 						,(real * c.imaginary) + (imaginary * c.real) };
+
 	}
-	
+
+	//Division
 	Complex operator/(const Complex& c)
 	{
 		Complex denominator = conjugate(c);
@@ -50,25 +52,25 @@ public:
 
 	friend ostream &operator<<( ostream& output, const Complex &c)
 	{
-		if (c.imaginary > 0)
-			output << setprecision(2) <<c.real << " + " << c.imaginary << "i";
-		else if (c.real != 0.0 && c.imaginary != 0.0)
+		if (c.real != 0.0 && c.imaginary != 0.0)
 		{
-			string a = to_string(c.imaginary);
-			for (int i = 0 ; i < a.length(); i++)
-			{
-				if (a[i] == '.')
-				{
-					a = (a[i + 1] != '0') ? a.substr(1, i + 2) : a.substr(1, i - 1);
-					break;
-				}
-					
-			}
-			output << setprecision(2) << c.real << " - " << a <<"i";
+			if (c.imaginary > 0)
+				output << setprecision(3)<<"(" << c.real << " + " << c.imaginary << "i)";
+			else
+				output << setprecision(3)<<"(" << c.real << " " << c.imaginary << "i)";
 		}
 		else
-			output << 0;
-		
+		{
+			if (c.real == 0.0 && c.imaginary != 0.0 && c.imaginary != 1)
+				output <<"(" << c.imaginary << "i)";
+			else if(c.real == 0.0 && c.imaginary == 1)
+				output << "i";
+			else if (c.real != 0.0 && c.imaginary == 0.0)
+				output << c.real;
+			else
+				output << "(0 + 0i)";
+		}
+
 		 return output;
 	}
 	 friend istream &operator>>(istream& input, Complex &c)
@@ -100,33 +102,81 @@ private:
 
 int main()
 {
-	Complex 	a(0,1),
-			b(2,3),
-			c(-4,5), 
-			d(4),
-			e(2.6,3.33);
+	string ans;
+	bool cond = false;
+	Complex x,y,z;
+	/*
+	Complex 
+		a{ 0,1 },
+		b(2, 3),
+		c(-4,5),
+		d(4, 0),
+		e(2.5, 3.33);
 
-	//cout << "Enter the complex number (real before imaginary): ";
-	//cin >> b; // 2 + 3i 
-	cout << "The complex number a is: " << a << endl;
-	cout << "The complex number b is: " << b << endl;
-	cout << "The complex number c is: " << c << endl;
-	
-	cout << "a + b: " << a + b << endl;
-	cout << "a * b: " << a * b << endl;
+	cout << "The complex number z : " << z << endl;
+	cout << "The complex number a : " << a << endl;
+	cout << "The complex number b : " << b << endl;
+	cout << "The complex number c : " << c << endl;
+	cout << "The complex number d : " << d << endl;
+	cout << "The complex number e : " << e << endl;
 
-	cout << "b - c: " << b - c << endl;
-	cout << "b / c: " << b / c << endl;
+	z = a + b;
+	cout << "\nz = a + b:\n" << z << " = " << a << " + " << b << endl;
+	z = b - c;
+	cout << "\nz = b - c:\n" << z << " = " << b << " - " << c << endl;
+	z = c * d;
+	cout << "\nz = c * d:\n" << z << " = " << c << " * " << d << endl;
+	z = d / e;
+	cout << "\nz = d / e:\n" << z << " = " << d << " / " << e << endl;
+	ans = (b == e) ? "True" : "False";
+	cout << "\nb == e: " << ans;
+	ans = (d != e) ? "True" : "False";
+	cout << "\nd != e: " << ans << endl;
+	ans = (c == c) ? "True" : "False";
+	cout << "\nc == c: " << ans;
+	ans = (c != c) ? "True" : "False";
+	cout << "\nc != c: " << ans;
 
-	if (a != b)
+	cout << endl;
+	*/
+	while (!cond)
 	{
-		cout << "a is different from b" << endl;
-	}
-	else if ( a == b)
-	{
-		cout << "a is the same with b" << endl;
-	}
+		Complex z( 0 ,0 );
+		cout << "Enter the complex number x (real before imaginary): ";
+		cin >> x; 
+		cout << "Enter the complex number y (real before imaginary): ";
+		cin >> y;
+
+		cout << "The complex number x : " << x << endl;
+		cout << "The complex number y : " << y << endl;
+		cout << "The complex number z : " << z << endl;
+
+		z = x + y;
+		cout << "\nz = x + y:\n" << z << " = " << x <<" + " << y <<endl;
+		z = x - y;
+		cout << "\nz = x - y:\n" << z << " = " << x << " - " << y << endl;
+		z = x * y;
+		cout << "\nz = x * y:\n" << z << " = " << x << " * " << y << endl;
+		z = x / y;
+		cout << "\nz = x / y:\n" << z << " = " << x << " / " << y <<endl;
+
+		ans = (x == y) ? "True" : "False";
+		cout << "\nx == y: " <<ans;
 	
+		ans = (x != y) ? "True" : "False";
+		cout << "\nx != y: " << ans << endl;
+
+		cout << "\nDo you want to continue (y/n)? ";
+		cin.clear(); cin.ignore(10000, '\n');
+		cin >> ans;
+		
+		if (ans == "y")
+			cond = false;
+		else if (ans == "n")
+			cond = true;
+		cout << endl;
+	}
+
 	system("pause");
 	return 0;
 }
